674651256de3db88477a2a6b903122b05fea99bd
ckdwlsrh <ckdwls6504@gmail.com>
2022-01-22 20:42:37 +0900

    todocontroller

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,9 +2,16 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+    @GetMapping("/list")
+    public String TodoList(@RequestParam(required = false) int id) {
+        return "hi";
+    }
     
 }
