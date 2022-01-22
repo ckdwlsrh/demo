a51fb9fd9cde88e6c8eb42a76d68e480e5b5fefe
ckdwlsrh <ckdwls6504@gmail.com>
2022-01-22 20:26:56 +0900

    test controller

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.TestRequestBodyDTO;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +23,9 @@ public class TestController {
     @GetMapping("/testRequestParam")
     public String testControllerWithRequestParam(@RequestParam(required = false) int id) {
         return "hello world id" + id;
+    }
+    @GetMapping("/testRequestBody")
+    public String testControllerWithRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "hello world id " + testRequestBodyDTO.getId() + "message : " + testRequestBodyDTO.getMessage();
     }
 }
