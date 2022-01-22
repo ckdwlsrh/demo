7fb4cecac180aee1942233c9063fda59b4eb1510
ckdwlsrh <ckdwls6504@gmail.com>
2022-01-22 21:50:48 +0900

    service & repository

---
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.persistence;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import com.example.demo.model.TodoEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String>{
+    @Query("select * from Todo t where t.userId = ?1")
+    List<TodoEntity> findByUserId(String userId);
+}
