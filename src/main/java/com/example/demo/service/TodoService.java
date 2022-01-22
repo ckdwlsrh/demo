b776cbe1db1c9029b9740d7de01ac1b12ee91e1d
ckdwlsrh <ckdwls6504@gmail.com>
2022-01-22 21:50:54 +0900

    service

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,10 +1,24 @@
 package com.example.demo.service;
 
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+
+    @Autowired
+    private TodoRepository repository;
+
     public String testService() {
-        return "hi test";
+        //todoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        repository.save(entity);
+
+        TodoEntity savedEntity = repository.findByUserId(entity.getId()).get(0);
+    
+        return savedEntity.getTitle();
     }
 }
