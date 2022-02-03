9a7e69106e4e635dc31e8165898567fb75a812f8
ckdwlsrh <ckdwls6504@gmail.com>
2022-02-03 22:02:15 +0900

    Create create method

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,11 +1,16 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -25,5 +30,25 @@ public class TodoService {
         
         //반환
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        //validation
+        if(entity == null) {
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if(entity.getUserid() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+
+        repository.save(entity);
+
+        log.info("Entity Id : {} is saved.", entity.getId());
+
+
+        return repository.findByUserid(entity.getUserid());
     }
 }
